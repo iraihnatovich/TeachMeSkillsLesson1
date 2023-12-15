5eeb83ebc6c5808e0edd77dc41fdf410f9a7c29d
IraIhnatovich <i.ihnatovich@gmai.com>
2023-12-15 15:41:49 +0300

    version 1.0

---
+++ src/by/teachmeskills/lesson1/HomeWorkExtra1.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.lesson1;
+
+import java.util.Scanner;
+
+public class HomeWorkExtra1 {
+    public static void main(String[] args) {
+        System.out.print("Enter your name, please: ");
+        Scanner str = new Scanner(System.in);
+        String username = str.next();
+        System.out.println("Hello "+ username);
+    }
+}
---
+++ src/by/teachmeskills/lesson1/MyApplication.java
@@ -0,0 +1,4 @@
+package by.teachmeskills.lesson1;
+
+public class MyApplication {
+}
